6a7333dd21b7d4a3872d35b66fae4e50f6fb3352
psso83 <pkse11283@g.hongik.ac.kr>
2026-01-08 12:09:57 +0900

    Chore : First commit

---
+++ src/test/java/com/example/hungry/HungryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hungry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HungryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
